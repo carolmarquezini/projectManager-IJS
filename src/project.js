14dc96be3aa4090ea6f5b3e55148b9653d7e2ac1
carolmarquezini <c.marquezini15@gmail.com>
2023-09-14 00:57:28 -0300

    Criando testes para classe Project e ajustando nomenclaturas.

--- src/project.js
+++ src/project.js
@@ -3,58 +3,78 @@ const { Engineer } = require('./engineer');
 
 class Project {
     #id;
-    responsavelTecnico;
-    endereco;
-    empreiteiro;
+    #technicalManager;
+    #architect
+    address;
+    serviceProvider;
     status;
-    manutencao;
+    listMaintenance = [];
 
-    constructor(id, responsavelTecnico, endereco, empreiteiro, status, manutencao) {
-        if (!(responsavelTecnico instanceof Engineer)) {
+    constructor(id, technicalManager, architect, address, serviceProvider, status, maintenanceList) {
+        if (!(technicalManager instanceof Engineer)) {
             throw new Error("Informe um responsável técnico válido!");
         }
 
-        if (!id || !responsavelTecnico || !endereco || !empreiteiro || !status) {
+        if (!(architect instanceof Architect)) {
+            throw new Error("Informe um responsável técnico válido!");
+        }
+
+        if (!id || !technicalManager || !address ) {
             throw new Error("Dados inválidos!");
         }
         this.#id = id;
-        this.name = name;
-        this.cpf = cpf;
-        this.contact = contact;
-        this.email = email;
+        this.#technicalManager = technicalManager;
+        this.#architect = architect;
+        this.address = address;
+        this.serviceProvider = serviceProvider;
+        this.status = status;
+        this.listMaintenance = this.listMaintenance;
+
+        if(maintenanceList){
+            this.listMaintenance.push(maintenanceList)
+        }
     }
 
     get id() {
         return this.#id;
     }
 
-    get responsavelTecnico() {
-        return this.responsavelTecnico;
+    get technicalManager() {
+        return this.#technicalManager;
     }
     
-    get endereco() {
-        return this.endereco;
+    get address() {
+        return this.address;
     }
     
-    set endereco(novoEndereco) {
-        return this.endereco = novoEndereco;
+    set address(newAddress) {
+        return this.address = newAddress;
     }
     
-    get empreiteiro() {
-        return this.empreiteiro;
+    get serviceProvider() {
+        return this.serviceProvider;
     }
     
-    set empreiteiro(nome) {
-        return this.empreiteiro = nome;
+    set serviceProvider(name) {
+        return this.serviceProvider = name;
     }
     
     get status() {
         return this.status;
     }
     
-    set status(novoStatus) {
-        return this.status = novoStatus;
+    set status(newStatus) {
+        return this.status = newStatus;
     }
+
+    addMaintenance(date, service){
+        this.listMaintenance.push({date,service});
+    }
+
+    getAllMaintenance(){
+        return this.listMaintenance;
+    }
+
 }
 
 module.exports = {
--- src/project.test.js
+++ src/project.test.js
@@ -2,20 +2,60 @@ const { Project } = require('./project')
 const { Engineer } = require('./engineer');
 const { Architect } = require('./architect')
 
-let architect = new Architect(1,'Carol', 123456, 9999999, '@gmail', 202320);
-let engineer = new Engineer(1,'Carol', 123456, 9999999, '@gmail', 202320);
-
-
+let architect = new Architect(1, 'Carol', 123456, 9999999, '@gmail', 202320);
+let engineer = new Engineer(1, 'Carol', 123456, 9999999, '@gmail', 202320);
 
 
 describe("Verificar Engenheiro(a)", () => {
-    // it("Retornar inválido caso não seja passado os atributos do construtor", () => {
-    //     //let user = new User(1,'Carol', 123456, 9999999, '@gmail');
-    //     expect(() => new Project(1, engineer, 'ipiranga', 'xxxxx', 'iniciando','nao aplicavel')
-    // })
+    it("Retornar inválido caso não seja passado o(a) responsável técnico(a)", () => {
+        expect(() => new Project(1, null, 'ipiranga', 'nao aplicavel')).toThrow("Informe um responsável técnico válido!");
+    });
 
-    it("Retornar válido quando todos atribudos do construtor são passados", () => {
-        let project = new Project(1, engineer, 'ipiranga', 'xxxxx', 'iniciando','nao aplicavel');
+    it("Retornar válido quando todos atribudos obrigatório do construtor são passados", () => {
+        let project = new Project(1, engineer, architect, 'ipiranga', 'nao aplicavel');
         expect(() => project).not.toThrow()
-    } )
+    })
 })
+
+describe("Teste do Setter de endereço", () => {
+    it("Deve definir um novo endereço corretamente", () => {
+        const projeto = new Project(1, engineer, architect, "Endereço Antigo", "Empreiteiro Antigo");
+        const novoEndereco = "Novo Endereço";
+        projeto.address = novoEndereco;
+        expect(projeto.address).toBe(novoEndereco);
+    })
+})
+
+describe("Teste do Setter de empreiteiro ", () => {
+    it(" Deve definir um novo empreiteiro corretamente ", () => {
+        const projeto = new Project(1, engineer, architect, "Endereço Antigo", "Empreiteiro Antigo");
+        const novoEmpreiteiro = "Novo empreiteiro";
+        projeto.serviceProvider = novoEmpreiteiro; 
+        expect(projeto.serviceProvider).toBe(novoEmpreiteiro)
+    })
+})
+
+describe("Teste do Setter de Status ", () => {
+    it(" Deve definir um novo status da obra corretamente ", () => {
+        const projeto = new Project(1, engineer,architect, "Endereço Antigo", "Empreiteiro Antigo", "Início");
+        const novoStatus = "em andamento";
+        projeto.status = novoStatus; 
+        expect(projeto.status).toBe(novoStatus)
+    })
+})
+
+describe('Teste do Setter de manutencao', () => {
+    it('Deve adicionar uma nova manutenção à lista de manutenções', () => {
+        
+        const project = new Project(1, engineer,architect, 'Endereço Antigo', 'Empreiteiro Antigo');
+        const maintenanceDate = '2023-09-13';
+        const maintenanceService = 'Reparo na estrutura';
+
+        project.addMaintenance(maintenanceDate, maintenanceService);
+
+        const maintenanceList = project.listMaintenance;
+
+        expect(maintenanceList).toHaveLength(1);
+        expect(maintenanceList[0]).toEqual({ date: maintenanceDate, service: maintenanceService });
+    });
+});
