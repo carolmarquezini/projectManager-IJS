3a3fb35186766a3f443ee4c7eb6fc4eb2a7d2a69
carolmarquezini <c.marquezini15@gmail.com>
2023-09-15 19:01:01 -0300

    Adicionando linha de final de arquivo e removendo comentário não utilizado

--- src/usuario.test.js
+++ src/usuario.test.js
@@ -2,7 +2,6 @@ const { Usuario } = require('./usuario')
 
 describe("Verificar Usuário", () => {
     it("Retornar inválido caso não seja passado os atributos do construtor", () => {
-        //let user = new User(1,'Carol', 123456, 9999999, '@gmail');
         expect(() => new Usuario('Carol', 123456, 9999999, '@gmail')).toThrow("Dados inválidos!")
     })
 
